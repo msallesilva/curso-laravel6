58838971d354d5aa29ae4262a091a54bcb3168e6
msallesilva <msallesilva@hotmail.com>
2020-03-12 12:23:26 -0300

    Rotas com Parametros

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,18 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/produtos/{idProdutos?}',function($idProduto = ''){
+    return "Produto(s)" . $idProduto;
+});
+
+Route::get('/categoria/{flag}/posts',function($flag){
+    return "Produtos da categoria : {$flag}";
+});
+
+Route::get('/categorias/{flag}',function($bla){
+    return "Produtos da categoria : {$bla}";
+});
+
 Route::match(['post','get'],'/match',function(){
     return "Match";
 });
